1f2b1a921a3ee7956dc48f122473f666408abcfe
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-24 18:08:02 +0500

    added changes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,7 +17,7 @@ const Hero = ({heroapi :{title, subtitle, btntext, img, sociallinks, videos}}) =
                         <Clips key={i} imgsrc={val.imgsrc} clip={val.clip}/>
                        ))}
                     </div>
-                    <div className=''></div>
+                    <div></div>
                 </div>
                 <div className=''>
                     <img src={img} alt="img" className='w-auto h-[45vh] lg:h-[35vh] md:h-[31vh] sm:h-[21vh] xsm:h-[19vh] transitions-theme -rotate-[25deg] hover:rotate-0 cursor-pointer object-fill' />
