7cbec763c23959f07fb33a3a6ee5d810206d2f2d
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-25 02:51:10 +0500

    added changes in footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,28 @@
 import React from "react";
-import { FlexContent, Hero, Sales, Stories } from "./components";
-import { heroapi, popularsales, toprateslaes, highlight, sneaker, story } from "./data/data.js";
+import { FlexContent, Footer, Hero, Sales, Stories } from "./components";
+import {
+  heroapi,
+  popularsales,
+  toprateslaes,
+  highlight,
+  sneaker,
+  story,
+  footerAPI
+} from "./data/data.js";
 
 const App = () => {
   return (
-    <main className="flex flex-col gap-16 relative">
-      <Hero heroapi={heroapi} />
-      <Sales endpoint={popularsales} ifExists />
-      <FlexContent endpoint={highlight} ifExists />
-      <Sales endpoint={toprateslaes} />
-      <FlexContent endpoint={sneaker} />
-      <Stories story={story}/>
-    </main>
+    <>
+      <main className="flex flex-col gap-16 relative">
+        <Hero heroapi={heroapi} />
+        <Sales endpoint={popularsales} ifExists />
+        <FlexContent endpoint={highlight} ifExists />
+        <Sales endpoint={toprateslaes} />
+        <FlexContent endpoint={sneaker} />
+        <Stories story={story} />
+      </main>
+      <Footer footerAPI={footerAPI} />
+    </>
   );
 };
 
