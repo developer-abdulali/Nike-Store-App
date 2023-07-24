698d0b9171486fa76b9c2ad861c8527d9cc547c1
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-24 19:34:41 +0500

    added chnges

--- src/components/utils/Item.jsx
+++ src/components/utils/Item.jsx
@@ -1,13 +1,30 @@
 import { StarIcon, ShoppingBagIcon } from "@heroicons/react/24/solid";
 import React from "react";
 
-const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
+const Item = ({
+  ifExists,
+  id,
+  color,
+  shadow,
+  title,
+  text,
+  img,
+  btn,
+  rating,
+  price,
+}) => {
   return (
     <>
       <div
-        className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center justify-items-center rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
+        className={`relative bg-gradient-to-b ${color} ${shadow} grid items-center ${
+          ifExists ? "justify-items-start" : "justify-items-center"
+        } rounded-xl py-4 px-5 transition-all duration-700 ease-in-out w-full hover:scale-105`}
       >
-        <div className="grid items-center justify-items-center">
+        <div
+          className={`grid items-center ${
+            ifExists ? "justify-items-start" : "justify-items-center"
+          }`}
+        >
           <h1 className="text-slate-200 text-xl lg:text-lg md:text-base font-medium filter drop-shadow">
             {title}
           </h1>
@@ -20,18 +37,40 @@ const Item = ({ id, color, shadow, title, text, img, btn, rating, price }) => {
             </div>
             <div className="flex items-center gap-1">
               <StarIcon className="icon-style w-5 h-5 md:w-4 md:h-4" />
-              <h1 className="md:text-sm font-normal text-slate-100">{rating}</h1>
+              <h1 className="md:text-sm font-normal text-slate-100">
+                {rating}
+              </h1>
             </div>
           </div>
         </div>
         <div className="flex items-center gap-3">
-          <button type="button" className="bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200">
+          <button
+            type="button"
+            className="bg-white/90 blur-effect-theme button-theme p-0.5 shadow shadow-sky-200"
+          >
             <ShoppingBagIcon className="icon-style text-slate-900" />
           </button>
-          <button type="button" className="bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black">{btn}</button>
+          <button
+            type="button"
+            className="bg-white/90 blur-effect-theme button-theme px-2 py-1 shadow shadow-sky-200 text-sm text-black"
+          >
+            {btn}
+          </button>
         </div>
-        <div className="flex items-center">
-          <img src={img} alt="img/item-img" className="h-36 w-64 transitions-theme hover:-rotate-12" />
+        <div
+          className={`flex items-center ${
+            ifExists ? "absolute top-5 right-1" : "justify-center"
+          }`}
+        >
+          <img
+            src={img}
+            alt={`img/item-img/${id}`}
+            className={`h-36 w-64 transitions-theme hover:-rotate-12 ${
+              ifExists
+                ? "h-auto w-64 lg:w-56 md:w-48 -rotate-[35deg]"
+                : "h-36 w-64"
+            }`}
+          />
         </div>
       </div>
     </>
