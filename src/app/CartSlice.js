8ee7259028c07509b83ba9b4a8b6c6687fabaaab
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-28 17:32:36 +0500

    added toaster

--- src/app/CartSlice.js
+++ src/app/CartSlice.js
@@ -1,29 +1,35 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { toast } from "react-hot-toast";
 const initialState = {
-    cartState: false,
-    cartItems: []  // like database
+  cartState: false,
+  cartItems: [], // like database
 };
 const CartSlice = createSlice({
-    initialState,
-    name :  "cart",
-    reducers :{
-        setOpenCart: (state, action) =>{
-            state.cartState = action.payload.cartState;
-        },
-        setCloseCart: (state, action) => {
-            state.cartState = action.payload.cartState;
-        },
-        setAddItemtoCard: (state, action) =>{
-            const itemIndex = state.cartItems.findIndex((item) => item.id === action.payload.id);
-            if(itemIndex >= 0){
-                state.cartItems[itemIndex].cartQuanity += 1;
-            } else{
-                const temp = {...action.payload, cartQuanity: 1}
-                state.cartItems.push(temp)
-            }
-        }
-    }
+  initialState,
+  name: "cart",
+  reducers: {
+    setOpenCart: (state, action) => {
+      state.cartState = action.payload.cartState;
+    },
+    setCloseCart: (state, action) => {
+      state.cartState = action.payload.cartState;
+    },
+    setAddItemtoCard: (state, action) => {
+      const itemIndex = state.cartItems.findIndex(
+        (item) => item.id === action.payload.id
+      );
+      if (itemIndex >= 0) {
+        state.cartItems[itemIndex].cartQuanity += 1;
+        toast.success(`Item QTY Increased`);
+      } else {
+        const temp = { ...action.payload, cartQuanity: 1 };
+        state.cartItems.push(temp);
+        toast.success(`${action.payload.title} Added to Cart`);
+      }
+    },
+  },
 });
-export const { setOpenCart, setCloseCart, setAddItemtoCard } = CartSlice.actions;
+export const { setOpenCart, setCloseCart, setAddItemtoCard } =
+  CartSlice.actions;
 export const selectCartState = (state) => state.cart.cartState;
 export default CartSlice.reducer;
