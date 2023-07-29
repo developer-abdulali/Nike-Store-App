87b4a48a10ba23a383024b79e34369c48faa66a5
developer-abdulali <ab.ali.soomro@gmail.com>
2023-07-29 15:41:51 +0500

    added changes

--- src/app/CartSlice.js
+++ src/app/CartSlice.js
@@ -37,17 +37,6 @@
 // export const selectCartState = (state) => state.cart.cartState;
 // export default CartSlice.reducer;
 
-
-
-
-
-
-
-
-
-
-
-
 import { createSlice } from "@reduxjs/toolkit";
 import { toast } from "react-hot-toast";
 
@@ -88,10 +77,10 @@ const CartSlice = createSlice({
         (item) => item.id === action.payload.id
       );
       if (itemIndex >= 0) {
-        state.cartItems[itemIndex].cartQuanity += 1;
+        state.cartItems[itemIndex].cartQuantity += 1;
         toast.success(`Item QTY Increased`);
       } else {
-        const temp = { ...action.payload, cartQuanity: 1 };
+        const temp = { ...action.payload, cartQuantity: 1 };
         state.cartItems.push(temp);
         toast.success(`${action.payload.title} Added to Cart`);
       }
@@ -100,7 +89,8 @@ const CartSlice = createSlice({
   },
 });
 
-export const { setOpenCart, setCloseCart, setAddItemtoCard } = CartSlice.actions;
-export const selectCartItems = (state) => state.cart.cartItems;
+export const { setOpenCart, setCloseCart, setAddItemtoCard } =
+  CartSlice.actions;
 export const selectCartState = (state) => state.cart.cartState;
+export const selectCartItems = (state) => state.cart.cartItems;
 export default CartSlice.reducer;
--- src/components/Cart.jsx
+++ src/components/Cart.jsx
@@ -1,15 +1,61 @@
+// import React from "react";
+// import CartCount from "./cart/CartCount";
+// import CartEmpty from "./cart/CartEmpty";
+// import CardItem from "./cart/CardItem";
+// import { useDispatch, useSelector } from "react-redux";
+// import { selectCartItems, selectCartState, setCloseCart } from "../app/CartSlice.js";
+
+// const Cart = () => {
+//   const dispatch = useDispatch();
+//   const ifCartState = useSelector(selectCartState);
+//   const cartItems = useSelector(selectCartItems)
+//   // console.log(cartItems)
+
+//   const onCartToggle = () => {
+//     dispatch(
+//       setCloseCart({
+//         cartState: false,
+//       })
+//     );
+//   };
+//   return (
+//     <>
+//       <div
+//         className={`fixed top-0 right-0 left-0 bottom-0 blur-effect-theme w-full h-full opacity-100 z-[250] ${
+//           ifCartState
+//             ? "opacity-100 visible translate-x-0"
+//             : "opacity-0 invisible translate-x-8"
+//         }`}
+//       >
+//         <div
+//           className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}
+//         >
+//           <CartCount onCartToggle={onCartToggle} />
+//           {/* {cartItems.length === 0 ? <CartEmpty /> : <cartItems />} */}
+//           {cartItems?.length === 0 ? <CartEmpty /> : <cartItems />}
+//         </div>
+//       </div>
+//     </>
+//   );
+// };
+
+// export default Cart;
+
 import React from "react";
 import CartCount from "./cart/CartCount";
 import CartEmpty from "./cart/CartEmpty";
-import CardItem from "./cart/CardItem";
+import CartItem from "./cart/CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { selectCartState, setCloseCart } from "../app/CartSlice.js";
+import {
+  selectCartItems,
+  selectCartState,
+  setCloseCart,
+} from "../app/CartSlice.js";
 
 const Cart = () => {
   const dispatch = useDispatch();
   const ifCartState = useSelector(selectCartState);
-  // const cartItems = useSelector(selectCartItems)
-  // console.log(cartItems)
+  const cartItems = useSelector(selectCartItems);
 
   const onCartToggle = () => {
     dispatch(
@@ -18,6 +64,7 @@ const Cart = () => {
       })
     );
   };
+
   return (
     <>
       <div
@@ -31,9 +78,11 @@ const Cart = () => {
           className={`blur-effect-theme h-screen max-w-xl w-full absolute right-0`}
         >
           <CartCount onCartToggle={onCartToggle} />
-          {/* <CartCount /> */}
-          <CartEmpty />
-          <CardItem />
+          {cartItems.length === 0 ? (
+            <CartEmpty />
+          ) : (
+            cartItems.map((item, i) => <CartItem key={i} item={item} />)
+          )}
         </div>
       </div>
     </>
@@ -41,3 +90,5 @@ const Cart = () => {
 };
 
 export default Cart;
+
+// ((item) => <CardItem key={item.id} item={item} />)
